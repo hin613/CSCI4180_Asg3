4d0134d2cbe276659d0948550f64e6aa7277c6f3
ken <ken@admins-MacBook-Pro.local>
2018-12-09 21:12:27 +0800

    upload done

--- Local.java
+++ Local.java
@@ -1,30 +1,35 @@
 import java.io.*;
 import java.util.*;
 import java.math.*;
-
+import java.lang.*;
 import java.nio.channels.FileChannel;
 import java.nio.ByteBuffer;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
-import java.lang.ClassNotFoundException;
+
 
 public class Local {
 
     public Local() {
-        File store = new File("store");
-        if (!store.exists()) {
-            store.mkdir();
-        }
+      File store = new File("store");
+      if (!store.exists()) {
+        store.mkdir();
+      }
     }
 
-    public static int exp_mod(int base, int exp, int modulus) {
-      base = base % modulus;
-      int result = 1;
-      while (exp > 0) {
-        if ((exp & 1) != 0)
-          result = (result * base) % modulus;
+    public int foo (int d, int exp, int q) {
+      return 1;
+    }
 
-        base = (base * base) % modulus;
+
+    public static int exp_mod(int d, int exp, int q) {
+      int result = 1;
+      d = d % q;
+      while (exp > 0) {
+        if ((exp & 1) > 0) {
+          result = (result * d) % q;
+        }
+        d = ((int) Math.pow(d, 2)) % q;
         exp = exp >> 1;
       }
       return result;
@@ -34,8 +39,8 @@ public class Local {
       if (fileName.exists()) {
         return true;
       } else {
-        System.err.println("File \"" + fileName.getName() + "\" not exist.");
-        // System.err.println("Try again!");
+        System.out.println("File \"" + fileName.getName() + "\" not exist.");
+        // System.out.println("Try again!");
         return false;
       }
     }
@@ -44,7 +49,7 @@ public class Local {
       if (fileName.mkdir()) {
         return true;
       } else {
-        System.err.println("[ERROR] Cannot create directory: \"" + fileName.getName() + "\"!");
+        System.out.println("[ERROR] Cannot create directory: \"" + fileName.getName() + "\"!");
         return false;
       }
     }
@@ -53,16 +58,16 @@ public class Local {
       if (fileName.isDirectory()) {
         return true;
       } else {
-        System.err.println("[ERROR] \"" + fileName.getName() + "\" is NOT a directory!");
+        System.out.println("[ERROR] \"" + fileName.getName() + "\" is NOT a directory!");
         return false;
       }
     }
 
     public void upload(int minChunk, int avgChunk, int maxChunk, int d, String fileToUpload, String storageType)
-      throws IOException, NoSuchAlgorithmException, ClassNotFoundException {
+      throws IOException, NoSuchAlgorithmException, ClassNotFoundException, NullPointerException {
         File file = new File(fileToUpload);
         if (!fExists(file)) {
-          System.err.println("[ERROR] Upload file NOT exists! Try again!");
+          System.out.println("[ERROR] Upload file NOT exists! Try again!");
           return;
         }
 
@@ -92,7 +97,7 @@ public class Local {
         }
 
         if (!fIsDirectory(dir)) {
-          // System.err.println("[ERROR] \"store\": not a directory");
+          // System.out.println("[ERROR] \"store\": not a directory");
           return;
         }
 
@@ -141,16 +146,14 @@ public class Local {
         // System.out.println(FileRecipeList.fileRecipes);
 
         if (FileRecipeList.fileRecipes.containsKey(fileToUpload)) {
-            System.err.println("[ERROR] File already exists!");
+            System.out.println("[ERROR] File already exists!");
         }
         recipesFileOut = new FileOutputStream(recipesFile.getAbsolutePath());
         recipesObjOut = new ObjectOutputStream(recipesFileOut);
 
 
         int maxBufferSize = (int) Runtime.getRuntime().freeMemory();
-        System.out.println(maxBufferSize);
-        System.out.println(fileSize);
-        int chunkIterations = (int) Math.ceil((1.0 * fileSize / maxBufferSize));      // count times to chunk large files
+        int chunkIterations = (int) Math.ceil(((float)fileSize / (float)maxBufferSize));
         int lastChunkSize = (int) (fileSize % maxBufferSize);
 
         long totalLogicChunks = 0;
@@ -159,9 +162,11 @@ public class Local {
         long totalUniqueFileBytes = 0;
         double spaceSaving = 0;
 
-        System.out.println("******* fileChunkSize ********");
+        // System.out.println("-----------------------------");
         for (int interation = 0; interation < chunkIterations; interation++) {
+
             byte[] fileBytes;
+
             if (interation == chunkIterations - 1) {
               fileBytes = new byte[lastChunkSize];
             } else {
@@ -171,8 +176,7 @@ public class Local {
             uploadFile.read(fileBytes);
 
             int fileChunkSize = fileBytes.length;
-            System.out.println(interation);
-            System.out.println(fileChunkSize);
+            // System.out.println(interation);
 
 
             int m = minChunk;
@@ -180,35 +184,46 @@ public class Local {
             int s = 0;
             int currentChunkSize = 0;
             int currentPos = 0;
-            int fp = 0;
+            int rfp = 0;
+            int testingFP = 0;
 
             List<Integer> offset = new ArrayList<>();
             List<Integer> chunkSize = new ArrayList<>();
 
             while ((currentPos + currentChunkSize <= fileChunkSize) && (currentPos + m <= fileChunkSize)) {
               if (s == 0) {
-                fp = 0;
+                rfp = 0;
                 for (int i = 1; i <= m; i++) {
-                  fp = (fp + ((int) (fileBytes[currentPos + i - 1] & 0xff) * exp_mod(d, m - i, q))) % q;
+                  // System.out.println("##################");
+                  rfp = (rfp + ( (int)(fileBytes[currentPos + i - 1] & 0xff) * exp_mod(d, m - i, q) )) % q;
+                  // System.out.println("currentPos + i - 1: " + (currentPos + i - 1));
+                  // System.out.println("fileBytes: " + fileBytes[currentPos + i - 1]);
+                  // System.out.println("fileBytes & 0xff: " + (fileBytes[currentPos + i - 1] & 0xff));
+                  // System.out.println("m - i: " + (m-i));
+                  testingFP += ((int)(fileBytes[currentPos + i - 1] & 0xff) * Math.pow(d, m-i)) % q;
+                  // System.out.println("rfp: " + rfp);
+                  // System.out.println("tFP: " + testingFP);
                 }
                 currentChunkSize = m;
               } else {
-                fp = (d * (fp - exp_mod(d, m - 1, q) * (int) (fileBytes[currentPos + s - 1] & 0xff)) + fileBytes[currentPos + s + m - 1]) % q;
-                while (fp < 0) {
-                  fp += q;
+                rfp = (d * (rfp - exp_mod(d, m - 1, q) * (int) (fileBytes[currentPos + s - 1] & 0xff)) + fileBytes[currentPos + s + m - 1]) % q;
+                while (rfp < 0) {
+                  rfp += q;
                 }
                 currentChunkSize++;
               }
+              // System.out.println("+++++++ RFP: " + rfp);
 
-              if ((fp & 0xFF) == 0 || currentChunkSize == maxChunk || currentPos + currentChunkSize >= fileChunkSize) {
-                offset.add(currentPos);
+              if ((rfp & 0xFF) == 0 || currentChunkSize == maxChunk || currentPos + currentChunkSize >= fileChunkSize) {
                 chunkSize.add(currentChunkSize);
+                offset.add(currentPos);
                 currentPos += currentChunkSize;
                 s = 0;
                 currentChunkSize = 0;
               } else {
                 s++;
               }
+
           }
 
           // handle special case: chunk smaller than m left behind
@@ -216,26 +231,46 @@ public class Local {
             offset.add(currentPos);
             chunkSize.add(fileChunkSize - currentPos);
           }
+
+
           if (offset.size() == chunkSize.size() && offset.size() > 0) {
             int numOfChunks = offset.size();
             // Do some file settings here
 
             for (int i = 0; i < numOfChunks; i++) {
               MessageDigest md = MessageDigest.getInstance("SHA-256");
-              md.update(Arrays.copyOfRange(fileBytes, offset.get(i), offset.get(i) + chunkSize.get(i)));
+              // System.out.println(offset.get(i));
+              // System.out.println(chunkSize.get(i));
+
+              // md.update(Arrays.copyOfRange(fileBytes, offset.get(i), offset.get(i) + chunkSize.get(i)));
+              for (int j = offset.get(i); j < (offset.get(i) + chunkSize.get(i)); j++) {
+                md.update(fileBytes[j]);
+              }
 
               byte[] messageDigest = md.digest();
 
               BigInteger no = new BigInteger(1, messageDigest);
 
-              String hashtext = no.toString(16);
+              String tempHashText = no.toString(16);
+              String remainHashText = null;
+              String hashtext;
 
-              while (hashtext.length() < 32) {
-                hashtext = "0" + hashtext;
+              // while (hashtext.length() < 32) {
+              //   hashtext = "0" + hashtext;
+              // }
+              if (tempHashText.length() < 32) {
+                for (int j = 0; j < (32 - tempHashText.length()); j++)
+                  remainHashText += "0";
+
+                hashtext = remainHashText.concat(tempHashText);
+              } else {
+                hashtext = tempHashText;
               }
 
+
               if (IndexList.index.containsKey(hashtext)) { // already have that chunk, reuse it
-                IndexList.index.get(hashtext).refCount += 1;
+                // IndexList.index.get(hashtext).refCount += 1;
+                IndexList.index.get(hashtext).refCount = IndexList.index.get(hashtext).refCount + 1;
                 fileRecipe.add(hashtext);
               } else {         // if no entry of this chunk in indexTable, do update, create new chunk
                 Index index = new Index();
@@ -252,40 +287,59 @@ public class Local {
                   IndexList.index.put(hashtext, new Index(chunkSize.get(i), 1));
                   fileRecipe.add(hashtext);
                 } else {
-                  System.err.println("Cannot create chunk file");
+                  System.out.println("Cannot create chunk file");
                   return;
                 }
               }
             }
-
           }
+
+
         }
-        System.out.println("******************************");
+        // System.out.println("-----------------------------");
 
         // report
-        for (String key : IndexList.index.keySet()) {
-          int refCount = IndexList.index.get(key).refCount;
-          int chunkSize = IndexList.index.get(key).chunkSize;
-          totalLogicChunks += refCount;
+
+
+
+
+        // for (String key : IndexList.index.keySet()) {
+        //   // int refCount = IndexList.index.get(key).refCount;
+        //   // int chunkSize = IndexList.index.get(key).chunkSize;
+        //   // totalLogicChunks += refCount;
+        //   totalUniqueChunks++;
+        //   totalLogicChunks += IndexList.index.get(key).refCount;
+        //
+        //   // totalLogicFileBytes += refCount * chunkSize;
+        //   totalLogicFileBytes += IndexList.index.get(key).refCount * IndexList.index.get(key).chunkSize;
+        //   // totalUniqueFileBytes += chunkSize;
+        //   totalUniqueFileBytes += IndexList.index.get(key).chunkSize;
+        // }
+        // spaceSaving = 1 - (1.0 * totalUniqueChunks / totalLogicChunks);
+
+
+        Iterator<String> it = IndexList.index.keySet().iterator();
+        while (it.hasNext()) {
+          String key = it.next();
           totalUniqueChunks++;
-          totalLogicFileBytes += refCount * chunkSize;
-          totalUniqueFileBytes += chunkSize;
+          totalLogicChunks += IndexList.index.get(key).refCount;
+          totalUniqueFileBytes += IndexList.index.get(key).chunkSize;
+          totalLogicFileBytes += IndexList.index.get(key).refCount * IndexList.index.get(key).chunkSize;
         }
-        spaceSaving = 1 - (1.0 * totalUniqueChunks / totalLogicChunks);
+        // spaceSaving = 1 - ((float)totalUniqueChunks / (float)totalLogicChunks);
 
         System.out.println("Total number of chunks in storage: " + totalLogicChunks);
         System.out.println("Number of unique chunks in storage: " + totalUniqueChunks);
         System.out.println("Number of bytes in storage with deduplication: " + totalUniqueFileBytes);
         System.out.println("Number of bytes in storage without deduplication: " + totalLogicFileBytes);
-        System.out.println("Space saving: " + spaceSaving);
+        System.out.println("Space saving: " + (1 - ((float)totalUniqueChunks / (float)totalLogicChunks)));
 
         FileRecipeList.fileRecipes.put(fileToUpload, fileRecipe);
 
         indexObjOut.writeObject(IndexList);
+        recipesObjOut.writeObject(FileRecipeList);
         indexObjOut.close();
         indexFileOut.close();
-
-        recipesObjOut.writeObject(FileRecipeList);
         recipesObjOut.close();
         recipesFileOut.close();
     }
@@ -295,6 +349,7 @@ public class Local {
 
     public void download(String fileToDownload, String storageType) throws IOException {
       String downloadedFileName = fileToDownload + ".download";
+
       try {
         FileRecipeList FileRecipeList = new FileRecipeList();
         List<String> fileRecipe = new ArrayList<>();
@@ -308,7 +363,6 @@ public class Local {
         if (!fExists(dir)) {
           if (!fMkdir(dir)) return;
         }
-
         if (!fIsDirectory(dir)) return;
 
 
@@ -321,6 +375,7 @@ public class Local {
 
         // index file
         indexFile = new File(dir.getName() + "/mydedup.index");
+        
         // isNewIndexFile = indexFile.createNewFile();
 
         // if (!isNewIndexFile) {
@@ -365,7 +420,7 @@ public class Local {
         fos = null;
       }
       catch (Exception e) {
-          System.err.println(e.getMessage());
+          System.out.println(e.getMessage());
       }
     }
 
@@ -403,9 +458,13 @@ public class Local {
 
           // index file
           indexFile = new File(dir.getName() + "/mydedup.index");
+
+
           if (!fExists(indexFile)) {
+            System.out.println("Creating index file...");
             indexFile.createNewFile();
           } else {
+            System.out.println("Index file exisits.");
             fileIn = new FileInputStream(indexFile.getAbsolutePath());
             objIn = new ObjectInputStream(fileIn);
             IndexList = (IndexList) objIn.readObject();
@@ -420,8 +479,10 @@ public class Local {
           // receipes file
           recipesFile = new File(dir.getName() + "/fileRecipes.index");
           if (!fExists(recipesFile)) {
+            System.out.println("Creating recipes file...");
             recipesFile.createNewFile();
           } else {
+            System.out.println("Recipes file exisits.");
             fileIn = new FileInputStream(recipesFile.getAbsolutePath());
             objIn = new ObjectInputStream(fileIn);
             FileRecipeList = (FileRecipeList) objIn.readObject();
@@ -460,7 +521,7 @@ public class Local {
 
         } catch (Exception e) {
           System.out.println("delete error");
-          System.err.println(e.getMessage());
+          System.out.println(e.getMessage());
         }
     }
 
